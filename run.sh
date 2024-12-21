445923cc8798af14c9134d75d8317decd7711916
fabricio <fabricio@node0.fabricio.demeter-pg0.clemson.cloudlab.us>
2024-12-21 12:32:09 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo ./build/src/c++/apps/app/psp-app --cfg ./configs/base_psp_cfg.yml --label test
+sudo numactl -N0 -m0 ./build/src/c++/apps/app/psp-app --cfg ./configs/base_psp_cfg.yml --label test
