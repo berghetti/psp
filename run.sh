b9831249f406e91420e166d8d212678ada4a83b4
fabricio <fabricio@node0.fabricio.demeter-pg0.clemson.cloudlab.us>
2024-12-21 12:50:43 -0700

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-sudo numactl -N0 -m0 ./build/src/c++/apps/app/psp-app --cfg ./configs/base_psp_cfg.yml --label test
+sudo numactl -N0 -m0 ./build/src/c++/apps/app/psp-app --cfg ./sosp_aec/configs/base_psp_cfg.yml --label test
